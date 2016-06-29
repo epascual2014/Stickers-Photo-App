eb01dd6e5c3e77cf67e2ed3baa3763192a34c964
Jorge Bastos <jbastosneto@me.com>
2016-06-29 10:51:06 -0700

    implemented Pan Gestures recognizer
    implemented the pan gesture recognizer in the stickerImageView class

---
+++ Sticker Photo App/Sticker Photo App/Helpers/PhotoTakingHelper.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoTakingHelper.swift
+//  Sticker Photo App
+//
+//  Created by Jorge Bastos on 6/29/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+typealias PhotoTakingCallback = UIImage? -> Void
+
+class PhotoTakingHelper {
+//	var photoTakingCallback: PhotoTakingCallback
+//	let viewController: UIViewController
+//	
+//	init(viewController: UIViewController) {
+//		self.viewController = viewController
+//	}
+}
--- Sticker Photo App/Sticker Photo App/stickerImageView.swift
+++ Sticker Photo App/Sticker Photo App/stickerImageView.swift
@@ -10,13 +10,44 @@ import UIKit
 
 class stickerImageView: UIImageView {
 
+	var viewController: UIViewController?
+	
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    override init(image: UIImage?) {
-        super.init(image: image)
+	override init(image: UIImage?) {
+		super.init(image: image)
+		self.userInteractionEnabled = true
+		
+		let pan = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(_:)))
+		self.addGestureRecognizer(pan)
     }
-    
+	
+//	convenience init(image: UIImage?, withViewController viewController: UIViewController? = nil) {
+//		
+//		if let viewController = viewController {
+//			self.viewController = viewController
+//			viewController.view.addSubview(self)
+//		}
+//		
+//		self.init(image: image)
+//		
+//		self.userInteractionEnabled = true
+//	}
 
+	func handlePan(panGestureRecognizer: UIPanGestureRecognizer!) {
+		if panGestureRecognizer.state == .Began || panGestureRecognizer.state == .Changed {
+			let translation = panGestureRecognizer.translationInView(panGestureRecognizer.view) // Get the spot it is now
+			
+			if let view = panGestureRecognizer.view {
+				let affinerTransformTranslate = CGAffineTransformTranslate(view.transform, translation.x, translation.y)
+				view.transform = affinerTransformTranslate
+				panGestureRecognizer.setTranslation(CGPointZero, inView: view)
+			} else {
+				print("No View")
+			}
+		}
+	}
+	
 }
