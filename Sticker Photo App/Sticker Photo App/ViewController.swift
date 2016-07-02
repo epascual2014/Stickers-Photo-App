948365fdbe390bdfd8f5ff2513a19834717a778d
Jorge Bastos <jbastosneto@me.com>
2016-07-01 19:44:20 -0700

    UI Inprovements

--- Sticker Photo App/Sticker Photo App/ViewController.swift
+++ Sticker Photo App/Sticker Photo App/ViewController.swift
@@ -25,32 +25,15 @@ class ViewController: UIViewController {
 	@IBOutlet weak var imageView: UIImageView!
 	@IBOutlet weak var imageCaptureButton: UIButton!
 	@IBOutlet weak var toolbar: UIToolbar!
+	@IBOutlet weak var doneButton: UIBarButtonItem!
     
     @IBAction func getImageTapped(sender: AnyObject) {
 		takePhoto()
     }
-    
-	func takePhoto() {
-		photoTakingHelper = PhotoTakingHelper(viewController: self) { (image: UIImage?) in
-			if let image = image {
-				self.imageView.image = image
-				self.toolbar.hidden = false
-				self.imageCaptureButton.hidden = true
-			}
-		}
-	}
-	
-	func showActivityController(sender: UIBarButtonItem) {
-		toolbar.hidden = true
-		imageView.image = photoTakingHelper.takeScreeShot(self.view!)
-		toolbar.hidden = false
-		let activityController = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
-		activityController.popoverPresentationController?.barButtonItem = sender
-		presentViewController(activityController, animated: true, completion: nil)
-	}
 	
 	//MARK: Toolbar Buttons
     @IBAction func stickerTapped(sender: UIBarButtonItem) {
+		
         let toolBarButtons = StickerName(rawValue: sender.tag)!
 		
         switch toolBarButtons {
@@ -61,64 +44,115 @@ class ViewController: UIViewController {
         case .Hat:
             hat(sender)
         case .Done:
-            done()
+            done(sender)
 		case .Activity:
 			showActivityController(sender)
         }
     }
 
 	func eyeglasses(sender: UIBarButtonItem) {
-        print(#function)
-        let eyeglassSticker = stickerImageView(image: UIImage(named: "eyeglasses1"))
-        eyeglassSticker.frame.origin = CGPoint(x: 50, y: 200)
-        self.view.addSubview(eyeglassSticker)
-        stickersArray.append(eyeglassSticker)
-		
-		// desable button after user
-		if sender.tag == 2 {
-			sender.enabled = false
+		if imageView.image != nil {
+			let eyeglassSticker = stickerImageView(image: UIImage(named: "eyeglasses1"))
+			eyeglassSticker.frame.origin = CGPoint(x: 50, y: 200)
+			self.view.addSubview(eyeglassSticker)
+			stickersArray.append(eyeglassSticker)
+			// desable button after user
+			if sender.tag == 2 {
+				sender.enabled = false
+			}
 		}
     }
 	
     func mustache(sender: UIBarButtonItem){
-        print(#function)
-        let mustacheSticker = stickerImageView(image: UIImage(named: "mustache3"))
-        mustacheSticker.frame.origin = CGPoint(x: 50, y: 50)
-        self.view.addSubview(mustacheSticker)
-        stickersArray.append(mustacheSticker)
-		
-		// desable mustache button after user
-		if sender.tag == 3 {
-			sender.enabled = false
+		if imageView.image != nil {
+			let mustacheSticker = stickerImageView(image: UIImage(named: "mustache3"))
+			mustacheSticker.frame.origin = CGPoint(x: 50, y: 50)
+			self.view.addSubview(mustacheSticker)
+			stickersArray.append(mustacheSticker)
+			// desable mustache button after user
+			if sender.tag == 3 {
+				sender.enabled = false
+			}
 		}
 	}
 	
     func hat(sender: UIBarButtonItem){
-        print(#function)
-        let hatSticker = stickerImageView(image: UIImage(named: "hat1"))
-        hatSticker.frame.origin = CGPoint(x: 50, y: 50)
-        //array.append(nameofarray)
-        self.view.addSubview(hatSticker)
-        stickersArray.append(hatSticker)
-		
-		// desable hat button after user
-		if sender.tag == 4 {
-			sender.enabled = false
+		if imageView.image != nil {
+			let hatSticker = stickerImageView(image: UIImage(named: "hat1"))
+			hatSticker.frame.origin = CGPoint(x: 50, y: 50)
+			//array.append(nameofarray)
+			self.view.addSubview(hatSticker)
+			stickersArray.append(hatSticker)
+			
+			// desable hat button after user
+			if sender.tag == 4 {
+				sender.enabled = false
+			}
 		}
     }
     
-    func done(){
-        print(#function)
-        for sticker in stickersArray {
-            sticker.userInteractionEnabled = false
-            self.toolbar.hidden = true
-        }
+	func done(sender: UIBarButtonItem){
+		if imageView.image != nil {
+			guard let barButtonIndex = toolbar.items?.indexOf(sender) else { return }
+			let actionBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: #selector(self.stickerTapped(_:)))
+			actionBarButtonItem.tag = 1
+			toolbar.items![barButtonIndex] = actionBarButtonItem
+			
+			disableStickers()
+		}
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+	func takePhoto() {
+		photoTakingHelper = PhotoTakingHelper(viewController: self) { (image: UIImage?) in
+			if let image = image {
+				self.imageView.image = image
+				self.toolbar.hidden = false
+				self.imageCaptureButton.hidden = true
+			}
+		}
+	}
+	
+	func hideStickers() {
+		for sticker in stickersArray {
+			sticker.hidden = true
+		}
+	}
+	
+	func disableStickers() {
+		for sticker in stickersArray {
+			sticker.userInteractionEnabled = false
+		}
+	}
+	
+	func resetState() {
+		imageCaptureButton.hidden = false
+		imageView.image = nil
+		toolbar.items![1].enabled = true
+		toolbar.items![3].enabled = true
+		toolbar.items![5].enabled = true
+	}
+	
+	func showActivityController(sender: UIBarButtonItem) {
+		toolbar.hidden = true
+		imageView.image = photoTakingHelper.takeScreeShot(self.view!)
+		hideStickers()
+		toolbar.hidden = false
+		let activityController = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+		activityController.popoverPresentationController?.barButtonItem = sender
+		activityController.completionWithItemsHandler = { (activity: String?, success: Bool, objectsToShare: [AnyObject]?, error: NSError?) in
+			print("activity \(activity), success \(success), \(objectsToShare), error \(error)")
+			
+			if !success {
+				self.resetState()
+			}
+		}
+		presentViewController(activityController, animated: true, completion: nil)
+	}
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
